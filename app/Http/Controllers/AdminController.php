bcc9f1fe58a20a524a9ff76797943d4c0243d834
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-07-08 19:58:41 +0700

    edit total revenue

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -224,7 +224,9 @@ public function buyerDetail(Request $request, $event_id)
 
     $orders = $query->get();
 
-    $totalRevenue = $orders->where('status', 'Success')->sum('total_amount')-4500;
+    $totalRevenue = $orders->where('status', 'Success')->sum(function ($order) {
+        return $order->total_amount - 4500;
+    });
 
     return view('admin.page.buyerDetail', compact('orders', 'totalRevenue'));
 }
