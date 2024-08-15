42ca44886550f560513368b05f96939ba87bf681
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-08-16 01:58:10 +0700

    modify footer

--- resources/views/landing/components/footer.blade.php
+++ resources/views/landing/components/footer.blade.php
@@ -3,53 +3,59 @@
 <footer class="bg-[#454545]">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
       <div class="md:flex md:justify-between">
-        <div class="mb-6 md:mb-0">
+        <div class="mb-2 md:mb-0">
           <a href="#" class="flex items-center">
             <span class="self-center text-5xl font-semibold whitespace-nowrap text-white">Ticketify</span>
           </a>
         </div>
-        {{-- <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+        <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
           <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Profile</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="#" class="hover:underline">Home</a>
+            <h2 class="mb-2 text-xl font-semibold text-gray-900 uppercase dark:text-white">Tentang Ticketify</h2>
+            <ul class="text-white font-medium">
+              <li class="">
+                <a href="#" class="hover:underline font-extralight">About Us</a>
               </li>
               <li>
-                <a href="#" class="hover:underline">Ticket</a>
+                <a href="#" class="hover:underline font-extralight">Syarat dan Ketentuan</a>
               </li>
               <li>
-                <a href="#" class="hover:underline">About Us</a>
+                <a href="#" class="hover:underline font-extralight">Kebijakan Privasi</a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline font-extralight">Pengembalian Dana</a>
               </li>
             </ul>
           </div>
           <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Services</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="#" class="hover:underline">Online Ticketing</a>
+            <h2 class="mb-2 text-xl font-semibold text-gray-900 uppercase dark:text-white">Services</h2>
+            <ul class="text-white font-medium">
+              <li class="">
+                <p href="#" class=" font-extralight">Online Ticketing</p>
               </li>
               <li>
-                <a href="#" class="hover:underline">Concert</a>
+                <p href="#" class=" font-extralight">Concert</p>
               </li>
               <li>
-                <a href="#" class="hover:underline">Event Management</a>
+                <p href="#" class=" font-extralight">Event Management</p>
               </li>
             </ul>
           </div>
           <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact Us</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="#" class="hover:underline">Ticketifyid@gmail.com</a>
+            <h2 class="mb-2 text-xl font-semibold text-gray-900 uppercase dark:text-white">Contact Us</h2>
+            <ul class="text-white font-medium">
+              <li class="">
+                <a href="mailto:Ticketifyid@gmail.com" class="hover:underline font-extralight">Ticketifyid@gmail.com</a>
+              </li>
+              <li class="">
+                <a href="https://wa.me/6282227031735" class="hover:underline font-extralight">+62 822-2703-1735</a>
               </li>
             </ul>
           </div>
-        </div> --}}
+        </div>
       </div>
-      <hr class="my-6 sm:mx-auto border-white lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-        <span class="text-sm sm:text-center text-gray-400">© 2024 <a href="#" class="hover:underline">Ticketify™</a>. All Rights Reserved. </span>
+      <hr class="my-2 sm:mx-auto border-white lg:my-4" />
+      <div class="sm:flex items-center justify-center">
+        <span class="text-center text-gray-400">Copyright © Ticketify 2024</span>
       </div>
     </div>
   </footer>
