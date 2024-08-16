6bcb7b255f180bbdc2f2963fa135d4c7ae4d06d1
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-08-16 13:30:16 +0700

    modify refund page

--- resources/views/landing/pages/refund.blade.php
+++ resources/views/landing/pages/refund.blade.php
@@ -3,41 +3,32 @@
 @section('content')
     <div class="p-4">
         <div class="flex justify-center">
-            <h1 class="font-semibold text-2xl mb-8 mt-4">About Us</h1>
+            <h1 class="font-semibold text-2xl mb-8 mt-4">Pengembalian Dana</h1>
         </div>
         <div class="lg:mx-20">
-            <p>Selamat datang di <span class="font-bold italic">Ticketify</span> Kami adalah platform penjualan tiket online
-                yang berkomitmen untuk menghubungkan Anda dengan acara-acara terbaik di seluruh Indonesia. Dari konser musik
-                dan festival hingga pertunjukan teater dan pertandingan olahraga, Ticketify adalah tempat tujuan Anda untuk
-                mendapatkan tiket dengan mudah, aman, dan cepat.</p>
-            <h2 class="mt-4 font-bold">Misi Kami</h2>
-            <p>Misi kami adalah untuk menyederhanakan proses pembelian tiket acara dan memberikan pengalaman terbaik bagi
-                pengguna kami. Kami percaya bahwa acara-acara luar biasa harus dapat diakses oleh semua orang, dan dengan
-                teknologi inovatif kami, kami memastikan bahwa Anda dapat membeli tiket dengan nyaman kapan saja dan di mana
-                saja.</p>
-            <h2 class="mt-4 font-bold">Apa yang kami Tawarkan?</h2>
-            <p class="mb-2">Beragam Acara: Kami menawarkan berbagai macam tiket untuk acara-acara populer di seluruh
-                Indonesia. Apakah
-                Anda penggemar musik, teater, atau olahraga, kami memiliki sesuatu untuk semua orang.
-                Proses Pembelian yang Mudah: Dengan antarmuka yang ramah pengguna, Anda dapat mencari, memilih, dan membeli
-                tiket hanya dengan beberapa klik saja.</p>
-            <p>Keamanan Transaksi: Keamanan Anda adalah prioritas kami. Kami bekerja sama dengan penyedia layanan pembayaran
-                terkemuka untuk memastikan bahwa setiap transaksi yang Anda lakukan melalui platform kami aman dan
-                terlindungi.
-                Dukungan Pelanggan yang Responsif: Tim dukungan pelanggan kami selalu siap membantu Anda dengan pertanyaan
-                atau masalah apa pun yang mungkin Anda hadapi. Kami berkomitmen untuk memberikan layanan pelanggan yang
-                cepat dan efisien.</p>
-            <h2 class="mt-4 font-bold">Mengapa Memilih Ticketify?</h2>
-            <p class="mb-2">Pengalaman dan Reputasi: Dengan pengalaman yang luas di industri tiket, kami memiliki rekam
-                jejak yang solid dalam memberikan layanan berkualitas kepada pelanggan kami.
-                Kredibilitas: Kami bekerja sama dengan penyelenggara acara resmi dan terverifikasi untuk memastikan bahwa
-                Anda hanya mendapatkan tiket asli.</p>
-            <p>Inovasi: Kami terus berinovasi untuk menghadirkan fitur-fitur terbaru yang memudahkan Anda dalam mengakses
-                acara favorit Anda.</p>
-            <h2 class="mt-4 font-bold">Tim Kami</h2>
-            <p>Di balik Ticketify terdapat tim profesional yang berdedikasi dan berpengalaman di bidang teknologi dan
-                industri hiburan. Kami adalah sekelompok individu yang bersemangat dan berdedikasi untuk membawa Anda lebih
-                dekat dengan acara-acara yang Anda cintai.</p>
+            <h2 class="mt-4 font-bold">Pengembalian Dana</h2>
+            <p>Kamu tidak bisa mengubah jenis kategori tiket menjadi yang lainnya, dan mengubah jadwal. Periksa dengan
+                teliti untuk pemilihan jenis kategori tiket pada pemesanan kamu.</p>
+            <h2 class="mt-4 font-bold">Pembatalan Tiket</h2>
+            <p class="mb-2">Perlu diketahui, tiket yang sudah kamu beli bersifat “Tidak dapat dibatalkan”. Jika event dan
+                atau acara dibatalkan oleh pihak promotor atau penyelenggara, maka kamu akan mendapatkan notifikasi berupa
+                whatsapp atau email dari ticketify.id untuk pengajuan refund. Jika produk dapat di refund, maka prosesnya
+                membutuhkan waktu 14 hari kerja sejak tanggal pengajuan refund dan atau sesuai dengan ketentuan promotor.
+                Pengembalian dana akan diberikan dalam bentuk transfer ke rekening bank masing-masing sesuai dengan
+                pengajuan form yang diberikan, apabila form berbeda dengan detil pelanggan, maka pengembalian dana tidak
+                dapat dilakukan.</p>
+
         </div>
     </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 @endsection
