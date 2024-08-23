fdc27ead481b74c9171a502d5d0ca285c66b9213
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-08-23 11:12:52 +0700

    update navbar

--- resources/views/landing/components/navbar.blade.php
+++ resources/views/landing/components/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class=" bg-[#454545] w-full z-20 top-0 start-0 border-b border-[#454545]">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <div class="max-w-screen-xl mx-auto p-4 flex items-center justify-center">
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset('assets/logo/logo.svg') }}" class="h-10" alt="Flowbite Logo" />
         </a>
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        {{-- <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <div class="relative">
                 <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" type="button"
                     class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center bg-amber-400 hover:bg-blue-700 focus:ring-blue-800">
@@ -61,8 +61,8 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-        </div>
-        @if (Auth::check())
+        </div> --}}
+        {{-- @if (Auth::check())
         <div class="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul
                 class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#454545] bg-[#454545]  border-gray-700 mx-auto">
@@ -80,7 +80,7 @@
                 </li>
             </ul>
         </div>
-        @endif
+        @endif --}}
 
     </div>
 </nav>
