3eb9a16a4c7218879c8ed9ac19ffe725557ab382
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-04 19:31:17 +0700

    comment field1-5

--- resources/views/landing/pages/event/page/order.blade.php
+++ resources/views/landing/pages/event/page/order.blade.php
@@ -233,7 +233,7 @@
                         </div>
                     </div>                    
                 </div>
-                <div class="relative z-0 w-full mt-5 mb-5 group">
+                {{-- <div class="relative z-0 w-full mt-5 mb-5 group">
                     <div class="mt-2">
                         <input type="Text" name="field1" id="field1"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -277,7 +277,7 @@
                     <label for="floating_users_email"
                         class="peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Field 5</label>
                     </div>
-                </div>
+                </div> --}}
                 @endif
                 <button type="submit" class="bg-[#454545] text-white px-4 py-2 rounded">Checkout</button>
             </form>
