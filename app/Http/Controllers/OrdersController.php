73bf1013b309a94b23a452a3ea159d92f5ba07cb
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-09-05 00:11:56 +0700

    adding validation email existing event

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -73,152 +73,161 @@ class OrdersController extends Controller
     }
 
     public function createInvoice(Request $request)
-    {
-        // $this->users_name = $request->input('name');
-        try {
-            // dd($request);
-            $first_name = $request->input('first_name');
-            $last_name = $request->input('last_name');
-            $phone_number = $request->input('phone_number');
-            $gender = $request->input('gender');
-            $birth_date = Carbon::createFromFormat('Y-m-d', $request->input('birth_date'))->format('Y-m-d');
-            // if event_type = health
-            $nik = $request->input('nik');
-            $blood_type = $request->input('blood_type');
-            $bib = $request->input('bib');
-            $community = $request->input('community');
-            $size_shirt = $request->input('size_shirt');
-            $urgen_contact = $request->input('urgen_contact');
-            $number_urgen_contact = $request->input('number_urgen_contact');
-            $relation_urgen_contact = $request->input('relation_urgen_contact');
-            $field1 = $request->input('field1');
-            $field2 = $request->input('field2');
-            $field3 = $request->input('field3');
-            $field4 = $request->input('field4');
-            $field5 = $request->input('field5');
+{
+    try {
+        $email_buyer = $request->input('email');
+        $event_id = $request->input('event_id');
 
-            $qty = $request->input('qty');
-            $price = $request->input('price');
-            $discountAmount = $request->input('discount_amount', 0);
+        $existingOrder = Order::where('email_buyer', $email_buyer)
+                                ->where('event_id', $event_id)
+                                ->first();
 
-            if ($discountAmount != 0) {
-                $discountCode = $request->input('discount_code');
-                $discount = \App\Models\Discount::where('code', $discountCode)->first();
-                if ($discount && $discount->used > 0) {
-                    $discount->used -= 1;
-                    $discount->save();
-                }
-            }
-
-            // $totalAmount = $qty * $price - $discountAmount;
-
-            if ($price != 0) {
-                $totalAmount = $qty * $price;
-            } else {
-                $totalAmount = $qty * $price;
-            }
-
-            if ($price != 0) {
-                $internetFee = (5 / 100) * $totalAmount;
-                $totalAmount += $internetFee;
-            } else {
-                $totalAmount = 0;
-            }
-
-            $no_transaction = 'Inv-' . (string) Str::uuid();
-            $order = new Order();
-            $order->no_transaction = $no_transaction;
-            $order->event_id = $request->input('event_id');
-            $order->external_id = $no_transaction;
-            $order->name_buyer = $request->input('name');
-            $order->email_buyer = $request->input('email');
-            $order->qty = $qty;
-            $order->price = $price;
-            $order->ticket_type = $request->input('ticket_type');
-            $order->event_name = $request->input('event_name');
-            $order->total_amount = $totalAmount -= $discountAmount;
-            $order->first_name = $first_name;
-            $order->last_name = $last_name;
-            if (Auth::check()) {
-                $order->email_auth = Auth::user()->email;
-            }
-            $order->phone_number = $phone_number;
-            $order->birth_date = $birth_date;
-            $order->gender = $gender;
-            // if event_type = health
-            $order->nik = $nik;
-            $order->blood_type = $blood_type;
-            $order->bib = $bib;
-            $order->community = $community;
-            $order->size_shirt = $size_shirt;
-            $order->urgent_contact = $urgen_contact;
-            $order->number_urgen_contact = $number_urgen_contact;
-            $order->relation_urgen_contact = $relation_urgen_contact;
-            $order->field1 = $field1;
-            $order->field2 = $field2;
-            $order->field3 = $field3;
-            $order->field4 = $field4;
-            $order->field5 = $field5;
-
-            if ($price == 0) {
-                $order->save();
-                $this->generateTicketUsers($order, $order->name_buyer, $order->event_id, $order->email_buyer, $order->first_name, $order->last_name, $order->phone_number, $order->birth_date, $order->gender);
-            } else {
-                $fees = [
-                    [
-                        'type' => 'Admin Fee',
-                        'value' => $internetFee,
-                    ],
-                    [
-                        'type' => 'Discount',
-                        'value' => -$discountAmount,
-                    ],
-                ];
-                $items = new InvoiceItem([
-                    'name' => $request->input('ticket_type') . ' ' . $request->input('event_name'),
-                    'price' => $price,
-                    'quantity' => $request->input('qty'),
-                ]);
-
-                $createInvoice = new CreateInvoiceRequest([
-                    'external_id' => $no_transaction,
-                    'amount' => $totalAmount,
-                    'invoice_duration' => 172800,
-                    'items' => [$items],
-                    'fees' => $fees,
-                ]);
-
-                $apiInstance = new InvoiceApi();
-                $generateInvoice = $apiInstance->createInvoice($createInvoice);
-                $order->invoice_url = $generateInvoice['invoice_url'];
-                $order->save();
-
-                $details = [
-                    'name' => $order->name_buyer,
-                    'uniqueCode' => $no_transaction,
-                    'tipe_ticket' => $order->ticket_type,
-                    'total_payment' => 'Rp. ' . number_format($totalAmount, 0, ',', '.'),
-                    'invoice_url' => $generateInvoice['invoice_url'],
-                    'event_name' => $order->event_name,
-                    'event_date' => Carbon::parse($order->event->event_start)->translatedFormat('j F Y'),
-                    'event_location' => $order->event->event_location,
-                    'total_ticket' => $order->qty,
-                ];
-                Mail::to($order->email_buyer)->send(new \App\Mail\ReminderPayments($details, null));
-            }
-
-            if (Auth::check()) {
-                return redirect(route('history'));
-            } elseif ($price == 0) {
-                Alert::success('Selamat!', 'Pendaftaran berhasil!, silahkan cek E-Mail anda');
-                return redirect(route('index'));
-            } else {
-                return redirect($generateInvoice['invoice_url']);
-            }
-        } catch (\Throwable $th) {
-            throw $th;
+        if ($existingOrder) {
+            Alert::error('Pendaftaran Gagal', 'Email sudah digunakan');
+            return redirect()->back()->with('error', 'Email ini sudah digunakan untuk memesan tiket di acara ini.');
         }
+
+        $first_name = $request->input('first_name');
+        $last_name = $request->input('last_name');
+        $phone_number = $request->input('phone_number');
+        $gender = $request->input('gender');
+        $birth_date = Carbon::createFromFormat('Y-m-d', $request->input('birth_date'))->format('Y-m-d');
+        $nik = $request->input('nik');
+        $blood_type = $request->input('blood_type');
+        $bib = $request->input('bib');
+        $community = $request->input('community');
+        $size_shirt = $request->input('size_shirt');
+        $urgen_contact = $request->input('urgen_contact');
+        $number_urgen_contact = $request->input('number_urgen_contact');
+        $relation_urgen_contact = $request->input('relation_urgen_contact');
+        $field1 = $request->input('field1');
+        $field2 = $request->input('field2');
+        $field3 = $request->input('field3');
+        $field4 = $request->input('field4');
+        $field5 = $request->input('field5');
+
+        $qty = $request->input('qty');
+        $price = $request->input('price');
+        $discountAmount = $request->input('discount_amount', 0);
+
+        if ($discountAmount != 0) {
+            $discountCode = $request->input('discount_code');
+            $discount = \App\Models\Discount::where('code', $discountCode)->first();
+            if ($discount && $discount->used > 0) {
+                $discount->used -= 1;
+                $discount->save();
+            }
+        }
+
+        if ($price != 0) {
+            $totalAmount = $qty * $price;
+        } else {
+            $totalAmount = $qty * $price;
+        }
+
+        if ($price != 0) {
+            $internetFee = (5 / 100) * $totalAmount;
+            $totalAmount += $internetFee;
+        } else {
+            $totalAmount = 0;
+        }
+
+        $no_transaction = 'Inv-' . (string) Str::uuid();
+        $order = new Order();
+        $order->no_transaction = $no_transaction;
+        $order->event_id = $event_id;
+        $order->external_id = $no_transaction;
+        $order->name_buyer = $request->input('name');
+        $order->email_buyer = $email_buyer;
+        $order->qty = $qty;
+        $order->price = $price;
+        $order->ticket_type = $request->input('ticket_type');
+        $order->event_name = $request->input('event_name');
+        $order->total_amount = $totalAmount -= $discountAmount;
+        $order->first_name = $first_name;
+        $order->last_name = $last_name;
+        if (Auth::check()) {
+            $order->email_auth = Auth::user()->email;
+        }
+        $order->phone_number = $phone_number;
+        $order->birth_date = $birth_date;
+        $order->gender = $gender;
+        $order->nik = $nik;
+        $order->blood_type = $blood_type;
+        $order->bib = $bib;
+        $order->community = $community;
+        $order->size_shirt = $size_shirt;
+        $order->urgent_contact = $urgen_contact;
+        $order->number_urgen_contact = $number_urgen_contact;
+        $order->relation_urgen_contact = $relation_urgen_contact;
+        $order->field1 = $field1;
+        $order->field2 = $field2;
+        $order->field3 = $field3;
+        $order->field4 = $field4;
+        $order->field5 = $field5;
+
+        if ($price == 0) {
+            $order->save();
+            $this->generateTicketUsers($order, $order->name_buyer, $order->event_id, $order->email_buyer, $order->first_name, $order->last_name, $order->phone_number, $order->birth_date, $order->gender);
+        } else {
+            // Logika untuk menangani pembayaran dengan Xendit atau lainnya
+            $fees = [
+                [
+                    'type' => 'Admin Fee',
+                    'value' => $internetFee,
+                ],
+                [
+                    'type' => 'Discount',
+                    'value' => -$discountAmount,
+                ],
+            ];
+            $items = new InvoiceItem([
+                'name' => $request->input('ticket_type') . ' ' . $request->input('event_name'),
+                'price' => $price,
+                'quantity' => $request->input('qty'),
+            ]);
+
+            $createInvoice = new CreateInvoiceRequest([
+                'external_id' => $no_transaction,
+                'amount' => $totalAmount,
+                'invoice_duration' => 172800,
+                'items' => [$items],
+                'fees' => $fees,
+            ]);
+
+            $apiInstance = new InvoiceApi();
+            $generateInvoice = $apiInstance->createInvoice($createInvoice);
+            $order->invoice_url = $generateInvoice['invoice_url'];
+            $order->save();
+
+            // Kirim email konfirmasi dengan link invoice
+            $details = [
+                'name' => $order->name_buyer,
+                'uniqueCode' => $no_transaction,
+                'tipe_ticket' => $order->ticket_type,
+                'total_payment' => 'Rp. ' . number_format($totalAmount, 0, ',', '.'),
+                'invoice_url' => $generateInvoice['invoice_url'],
+                'event_name' => $order->event_name,
+                'event_date' => Carbon::parse($order->event->event_start)->translatedFormat('j F Y'),
+                'event_location' => $order->event->event_location,
+                'total_ticket' => $order->qty,
+            ];
+            Mail::to($order->email_buyer)->send(new \App\Mail\ReminderPayments($details, null));
+        }
+
+        if (Auth::check()) {
+            return redirect(route('history'));
+        } elseif ($price == 0) {
+            Alert::success('Selamat!', 'Pendaftaran berhasil!, silahkan cek E-Mail anda');
+            return redirect(route('index'));
+        } else {
+            return redirect($generateInvoice['invoice_url']);
+        }
+    } catch (\Throwable $th) {
+        throw $th;
     }
+}
+
 
     public function notificationCallback(Request $request)
     {
